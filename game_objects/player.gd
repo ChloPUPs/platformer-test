00f05109868df3c6f96f6e01589bdd8984f6221a
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-04-15 07:57:48 -0700

    add double jumps

--- game_objects/player.gd
+++ game_objects/player.gd
@@ -3,15 +3,21 @@ extends CharacterBody2D
 const JUMP_STRENGTH = 600.0
 const SPEED: float = 400.0
 const GRAVITY_MULT: float = 1.3
+const MAX_AIRJUMP_AMOUNT: int = 1
+var airjumps_left: int = MAX_AIRJUMP_AMOUNT
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
+	if event.is_action_pressed("jump") and airjumps_left > 0:
 		velocity.y = -JUMP_STRENGTH
+		airjumps_left -= 1
 
 func _physics_process(delta: float) -> void:
 	var direction: float = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
 	if !is_on_floor():
 		velocity += get_gravity() * GRAVITY_MULT * delta
+	
+	if is_on_floor() and airjumps_left != MAX_AIRJUMP_AMOUNT:
+		airjumps_left = MAX_AIRJUMP_AMOUNT
 	
 	move_and_slide()
