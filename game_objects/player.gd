e90081cf2e70c2064b2fe808a8f3c5725fc71f5a
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-04-16 12:16:05 -0700

    add basic ui

--- game_objects/player.gd
+++ game_objects/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-const JUMP_STRENGTH = 800.0
+const JUMP_STRENGTH: float = 800.0
 const SPEED: float = 400.0
-const GRAVITY_STATES = { "jump": 1.7, "fall": 2.2 }
-const TERMINAL_VELOCITY = 1400.0
+const GRAVITY_STATES := { "jump": 1.7, "fall": 2.2 }
+const TERMINAL_VELOCITY: float = 1400.0
 const MAX_AIRJUMP_AMOUNT: int = 1
 var airjumps_left: int = MAX_AIRJUMP_AMOUNT
-var current_gravity_mult = GRAVITY_STATES["fall"]
+var current_gravity_mult: float = GRAVITY_STATES["fall"]
+var elapsed_since_movement: Dictionary[String, float] = {}
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and airjumps_left > 0:
@@ -15,6 +16,12 @@ func _input(event: InputEvent) -> void:
 		current_gravity_mult = GRAVITY_STATES["jump"]
 	if event.is_action_released("jump") or velocity.y > -10.0:
 		current_gravity_mult = GRAVITY_STATES["fall"]
+	if event.is_action_pressed("move_left"):
+		elapsed_since_movement["left"] = 0.0
+
+func _process(delta: float) -> void:
+	for key: String in elapsed_since_movement.keys():
+		elapsed_since_movement[key] += delta
 
 func _physics_process(delta: float) -> void:
 	var direction: float = Input.get_axis("move_left", "move_right")
@@ -34,3 +41,5 @@ func _apply_gravity(delta: float) -> void:
 
 func die() -> void:
 	print("You died I think why are you still moving")
+	%DeathUI.visible = true
+	%GameplayUI.visible = false
