2f26ef8e454d9fee2c5d0318443d1967cf01fe1c
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-04-16 10:16:29 -0700

    the best commit

--- game_objects/player.gd
+++ game_objects/player.gd
@@ -24,6 +24,7 @@ func _physics_process(delta: float) -> void:
 		if velocity.y > TERMINAL_VELOCITY:
 			velocity.y = TERMINAL_VELOCITY
 	
+	# jump jump jump suhvfjukiobsejh kjals
 	if is_on_floor() and airjumps_left != MAX_AIRJUMP_AMOUNT:
 		airjumps_left = MAX_AIRJUMP_AMOUNT
 	
