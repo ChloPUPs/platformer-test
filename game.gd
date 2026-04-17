7dcf8115f927bc6a94b5aa3da2b906df578ef9b5
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-04-17 10:51:08 -0700

    change fullscreen input to be in Game autoload

--- game.gd
+++ game.gd
@@ -5,3 +5,13 @@ enum GameState {
 	DEATH,
 }
 var game_state := GameState.GAMEPLAY
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		var window_mode := DisplayServer.window_get_mode()
+		var is_window: bool = window_mode != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(
+			DisplayServer.WINDOW_MODE_FULLSCREEN
+			if is_window
+			else DisplayServer.WINDOW_MODE_WINDOWED
+		)
